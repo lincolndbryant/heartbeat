05a5490fd0ed027cc30e6fadf4783134419433f2
lincolnbryant <bryant.lincoln@gmail.com>
2020-05-25 15:14:03 -0700

    compress sounds

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
 import heartSrc from "./img/heart.svg";
 import ShapeContainer from "./components/ShapeContainer";
-import { playSound } from "./lib/audio";
+import {playSound, startBeat, sequencePromise} from "./lib/audio";
 
 function App() {
+  const [beat, setBeat] = useState();
+  useEffect(() => {
+    sequencePromise.then(setBeat)
+  }, []);
+
   return (
-    <main className="app">
-      <img className="heart-bg" src={heartSrc} alt="" />
-      <ShapeContainer playSound={playSound} />
-    </main>
+    <>
+      <nav>
+        <button onClick={startBeat}>Start</button>
+      </nav>
+      <main className="app">
+        <img className="heart-bg" src={heartSrc} alt="" />
+        <ShapeContainer playSound={playSound} />
+      </main>
+    </>
   );
 }
 
--- src/lib/audio.js
+++ src/lib/audio.js
@@ -1,19 +1,75 @@
-import Tone from "tone";
+import { Sampler, Sequence, Player, Transport } from "tone";
 
-const sampler = new Tone.Sampler(
+const __BMIN_SAMPLES =
   {
-    G3: "G3_hard.mp3",
-    D3: "D3_hard.mp3",
-  },
+    "F#2": "PAN_02_Pan_Drum_Fs.mp3",
+    "A3": "PAN_03_Pan_Drum_A.mp3",
+    "C#4": "PAN_04_Pan_Drum_Cs.mp3",
+    "E4": "PAN_05_Pan_Drum_E.mp3",
+    "F#4": "PAN_07_Pan_Drum_Fs.mp3",
+   // "D5": "PAN_06_Pan_Drum_D.mp3",
+
+
+    "B4": "PAN_09_Pan_Drum_B_Soft.mp3",
+    "D4": "PAN_08_Pan_Drum_D_Soft.mp3",
+    "F5": "PAN_01_Pan_Drum_F_Soft.mp3",
+
+  };
+
+const DMIN_SAMPLES =
+  {
+    "D3": "PAN_15_Pan_Drum_D.mp3",
+    "A#4": "PAN_16_Pan_Drum_Bb.mp3",
+    "D4": "PAN_17_Pan_Drum_D.mp3",
+    "F4": "PAN_18_Pan_Drum_F.mp3",
+    "D5": "PAN_19_Pan_Drum_A.mp3",
+  };
+
+const samples = DMIN_SAMPLES;
+
+const sampler = new Sampler(samples,
   () => {
     console.log("sampler ready");
   },
-  "/samples/"
+  "/samples-mp3/"
 ).toMaster();
 
-const letters = ["A", "B", "C", "D", "E", "F", "G"];
+sampler.volume.value = -6;
+
+const drum = new Player('/samples-mp3/drum.mp3').toMaster();
+drum.volume.value = -12;
+
+const letters = ["D3", "F3", "G3", "A4", "A#4", "C#4", "D4", "F4", "G4", "A#5", "D5"];
+
+let playing = false;
+
+export function startBeat() {
+  if (playing) {
+    Transport.pause();
+    playing = false;
+  } else {
+    drum.sync();
+    Transport.start();
+    playing = true;
+  }
+}
 
 export function playSound(i) {
-  const letter = letters[i % letters.length];
-  sampler.triggerAttack(letter + "3");
+  const sampleCount = Object.keys(samples).length;
+  let note = Object.keys(samples)[i % sampleCount];
+  if (true) {
+    note = letters[i % letters.length];
+  }
+
+  sampler.triggerAttack(note);
 }
+
+export const sequencePromise = new Promise((resolve) => {
+  new Sequence(function (time, note) {
+    drum.volume.value = -6 - (note * 2);
+    drum.start(0);
+    resolve(note);
+  }, [1, 2, 3, 4]).start(0);
+});
+
+Transport.bpm.value = 50;
